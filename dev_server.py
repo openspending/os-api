467af36e908ae7cbdd8077e2c2f590d4a0ff6c1a
Adam Kariv <adam.kariv@gmail.com>
2016-01-18 13:23:26 +0200

    Optional memcached caching to requests

--- dev_server.py
+++ dev_server.py
@@ -1,6 +1,4 @@
-from os_api.app import create_app
-
-app = create_app()
+from os_api.app import app
 
 app.run(port=8000, debug=True)
 
--- os_api/app.py
+++ os_api/app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+import os
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+
+from flask import Flask, request
 from flask.ext.cors import CORS
 
 from babbage.api import configure_api as configure_babbage_api
@@ -25,3 +31,30 @@ def create_app():
 
 
 app = create_app()
+
+cache = None
+cache_timeout = None
+if 'OS_API_CACHE' in os.environ:
+    from werkzeug.contrib.cache import MemcachedCache
+    cache = MemcachedCache([os.environ['OS_API_CACHE']])
+    cache_timeout = int(os.environ.get('OS_API_CACHE_TIMEOUT',3600))
+
+@app.before_request
+def return_cached():
+    # if GET and POST not empty
+    if cache is not None:
+        o = urlparse(request.url)
+        key = o.path+'?'+o.query
+        response = cache.get(key)
+        if response:
+            response.from_cache = True
+            return response
+
+
+@app.after_request
+def cache_response(response):
+    if cache is not None and response.status_code == 200 and not hasattr(response, 'from_cache'):
+        o = urlparse(request.url)
+        key = o.path+'?'+o.query
+        cache.set(key, response, cache_timeout)
+    return response
