dc4e4415e158be571c0ea605d13dcbe2e0a7a612
Adam Kariv <adam.kariv@gmail.com>
2017-02-06 17:37:32 +0200

    Use os-api-cache package

--- os_api/cache.py
+++ os_api/cache.py
@@ -1,16 +1,16 @@
-import os
 from urllib.parse import urlparse
 
-import logging
 from flask import current_app, request, url_for
 
-from .caching.redis_cache import RedisCache
+from os_api_cache import get_os_cache
 
 
 def service_for_path(path):
     for x in {'/aggregate', '/members/', '/facts', '/package', '/model', '/loader'}:
         if x in path:
-            return path.split(x)[0], x.replace('/', '')
+            package_id = path.split(x)[0].split('/')[-1]
+            service = x.replace('/', '')
+            return package_id, service
     return None, None
 
 
@@ -21,17 +21,18 @@ def return_cached():
 
     o = urlparse(request.url)
     stats.increment('openspending.api.requests')
-    context, service = service_for_path(o.path)
+    package_id, service = service_for_path(o.path)
     if service is not None:
         stats.increment('openspending.api.requests.' + service)
 
     if cache is not None \
             and not (loader and o.path.startswith(url_for('FDPLoader.load'))):
-        response = cache.get(context, {'q': o.query, 'p': o.path})
+        response = cache.get_from_cache(package_id, o.query, o.path)
         if response:
             stats.increment('openspending.api.cache.hits')
             response.from_cache = True
-            response.headers.add('X-OpenSpending-Cache','true')
+            response.headers.add('X-OpenSpending-Cache', 'true')
+            response.headers.add('X-OpenSpending-PackageId', package_id)
             return response
         stats.increment('openspending.api.cache.misses')
 
@@ -44,22 +45,17 @@ def cache_response(response):
     stats.increment('openspending.api.responses.%d' % response.status_code)
 
     if cache is not None and response.status_code == 200 and not hasattr(response, 'from_cache'):
-        context, _ = service_for_path(o.path)
-        if context is not None:
-            cache.put(context, {'q': o.query, 'p': o.path}, response)
+        package_id, _ = service_for_path(o.path)
+        if package_id is not None:
+            cache.put_in_cache(package_id, o.query, o.path, response)
             response.headers.add('X-OpenSpending-Cache', 'false')
+            response.headers.add('X-OpenSpending-PackageId', package_id)
     return response
 
 
 def setup_caching(app):
-    if 'OS_API_CACHE' in os.environ:
-        host = os.environ.get('OS_API_CACHE')
-        cache_timeout = int(os.environ.get('OS_API_CACHE_TIMEOUT', 86400))
-        cache = RedisCache(host, 6379, cache_timeout)
-
-        logging.error('CACHE %s', cache)
-        logging.error('CACHE TIMEOUT %s', cache_timeout)
-
+    cache = get_os_cache()
+    if cache is not None:
         app.extensions['cache'] = cache
 
         app.before_request(return_cached)
