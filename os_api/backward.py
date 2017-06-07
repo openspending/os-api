8148b8899d3fb3c4948e7fecd4ccc98d403e97bf
Adam Kariv <adam.kariv@gmail.com>
2017-06-07 11:55:32 +0300

    Fix dataset naming

--- os_api/backward.py
+++ os_api/backward.py
@@ -102,7 +102,7 @@ def backward_compat_aggregate_api():
     pagesize = request.args.get('pagesize', 10000, int)
     page = request.args.get('page', 1, int)
 
-    dataset = '6018ab87076187018fc29c94a68a3cd2:' + dataset
+    dataset = '6018ab87076187018fc29c94a68a3cd2:__os_imported__' + dataset
     log.info('AGGREGATE dataset:%s', dataset)
 
     # Check if the dataset even exists
