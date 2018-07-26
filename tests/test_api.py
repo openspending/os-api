6c8eee1c1c30e34f642c820e2b8c1527e881f911
Brook Elgie <specialbrew@gmail.com>
2018-07-26 09:30:49 +0100

    Add dpp

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,10 +2,6 @@ import zipfile
 import json
 import six
 import pytest
-import os
-import logging
-
-from os_api import config
 
 
 class TestAPI(object):
@@ -21,7 +17,7 @@ class TestAPI(object):
     def test_package_inspection_configured_success(self, client):
         res = client.get('/api/3/info/__testing:ukgov-finances-cra/package')
         assert res.status_code == 200
-        assert res.json['name'] == 'ukgov_finances_cra'
+        assert res.json['name'] == 'ukgov-finances-cra'
 
     def test_package_inspection_configured_notfound(self, client):
         res = client.get('/api/3/info/__testing:no-package-here/package')
